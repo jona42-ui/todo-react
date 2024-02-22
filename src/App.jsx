bd1189a9f454e445de2a1f01c519f8498e7c8995
jona42-ui <jonathanthembo123@gmail.com>
2024-02-22 09:37:11 +0300

    feature

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+
+const [tasks, setTasks] = useState(props.tasks);
+
+function App(props) {
+
+  function addTask(name) {
+    const newTask = { id:`todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const taskList = tasks?.map((task) => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={ toggleTaskCompleted} deleteTask = {deleteTask} />);
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      //if this task has the same ID as the ediated task
+      if (id === task.id) {
+        //use object spread to make a new object
+        //whose `completed` prop has been inverted
+        return {
+          ...task, completed: !task.completed
+        };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+   const remainingTasks = tasks.filter((task)=> id !== task.id);
+   setTasks(remainingTasks);
+  }
+
+  const taskNoun = taskList.lenth !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} tasks remaining`
+  
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask= {addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
